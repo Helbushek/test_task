3fb97fd4876639e5b1789827a71edd0893b82279
Helbushek <gorevaya134@gmail.com>
2025-01-30 14:32:59 +0700

    final fix for linux

--- program1/main.cpp
+++ program1/main.cpp
@@ -5,7 +5,6 @@
 #include <string>
 #include <queue>
 #include <algorithm>
-#include <locale>
 #include <clocale>
 #include <cstring>
 #include <atomic>
@@ -13,7 +12,7 @@
 
 #ifdef _WIN32
     #include <winsock2.h>
-    #pragma comment(lib, "ws2_32.lib")
+    #pragma comment(lib, "Ws2_32.lib")
 #else
     #include <sys/socket.h>
     #include <netinet/in.h>
@@ -31,8 +30,8 @@ extern "C" {
 std::mutex m;
 std::condition_variable cv;
 std::queue<std::string> buffer;
-bool exit_flag = false;
-SOCKET sock;
+std::atomic<bool> exit_flag(false);
+int sock = -1;
 sockaddr_in in_addr{};
 
 bool reconnect() {
@@ -44,41 +43,46 @@ bool reconnect() {
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
-        std::cerr << "Error: Can`t establish socket connection. Retrying..." << std::endl;
         return false;
     }
     
     in_addr.sin_family = AF_INET;
     in_addr.sin_port = htons(8080);
-    in_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    #ifdef _WIN32
+        in_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    #else
+        inet_pton(AF_INET, "127.0.0.1", &in_addr.sin_addr);
+    #endif
 
     while (!exit_flag) {
         if (connect(sock, (sockaddr*)&in_addr, sizeof(in_addr)) != -1) {
-            std::cout << "Connected to server" << std::endl;
+            std::cout << "[-] SERVER: Connected to server" << std::endl;
             return true;
         }
-        std::cerr << "Error: Connection to the server is lost, please wait. Reconnecting..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(1));   
     }
     return false;
 }
 
 void receiver() {
+    std::cout << "[-] Receiving user input" << std::endl;
     while (!exit_flag) {
         std::string input;
         if (!std::getline(std::cin, input)) {
+            std::cerr << "[-] ERROR: Couldn`t complete input" << std::endl;
             exit_flag = true;
             break;
-        }
-
+        }        
+        
         if (input.length() > 64) {
-            std::cerr << "Error: Max length is 64 symbols\n";
-            continue;
+            std::cerr << "[-] ERROR: Max length is 64 symbols\n";
+            continue;   
         }
         bool valid = std::all_of(input.begin(), input.end(), [](char c) { return std::isdigit(c); });
 
-        if (!valid) {
-            std::cerr << "Error: Only digits allowed\n";
+        if (!valid || input.length() == 0) {
+            std::cerr << "[-] ERROR: Only digits allowed\n";
             continue;
         }
         std::vector<char> cstr(input.size()+1);
@@ -93,25 +97,56 @@ void receiver() {
     }
 }
 
+bool is_socket_connected() {
+    char buff;
+    int err = recv(sock, &buff, 1, MSG_PEEK | MSG_DONTWAIT);
+    if (err == 0) {
+        return false;
+    }
+    if (err == -1) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            return true;
+        }
+        return false;
+    }
+    return true;
+}
+
 void sender() {
     while (!exit_flag) {
         std::unique_lock<std::mutex> lock(m);
         cv.wait(lock, [] { return !buffer.empty() || exit_flag; });
         if (exit_flag && buffer.empty()) break;
-        if (!buffer.empty()) {
+        while (!buffer.empty()) {
             std::string data = buffer.front();
             buffer.pop();
             lock.unlock();
 
+            std::cout << "[-] BUFFER: " << data << std::endl;
+
             int sum = function2(data.c_str());
             std::string s = std::to_string(sum);
 
-            bool sent;
+            bool sent = false;
             while(!sent && !exit_flag) {
-                sent = false;
+                if (!is_socket_connected()) {
+                    std::cerr << "[-] SERVER: Connection lost" << std::endl;
+                    if (!reconnect()) {
+                        std::this_thread::sleep_for(std::chrono::seconds(1));
+                    }
+                    else {
+                        std::lock_guard<std::mutex> lock(m);
+                        while(!buffer.empty()) {
+                            buffer.pop();
+                        }
+                    }
+                }
+
+
                 int res = send(sock, s.c_str(), s.size(), 0);
 
                 if (res == -1) {
+                    std::cerr << "[-] SERVER: Connection lost" << std::endl;
                     if (!reconnect()) {
                         std::this_thread::sleep_for(std::chrono::seconds(1));
                     }
@@ -119,7 +154,7 @@ void sender() {
                 else {
                     sent = true;
                 }
-            }   
+            }
         }
     }
 }
@@ -133,7 +168,7 @@ int main() {
     #endif
 
     while(!reconnect()) {
-        std::cerr << "Failed to connect or lost connection to server, retrying...";
+        std::cerr << "[-] SERVER: Failed to connect to server, retrying...";
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
--- program2/main.cpp
+++ program2/main.cpp
@@ -3,7 +3,6 @@
 #include <thread>
 #include <queue>
 #include <string>
-#include <atomic>
 #include <clocale>
 
 #ifdef _WIN32
@@ -22,16 +21,16 @@ extern "C" {
     bool function3(const char* str);
 }
 
-std::atomic<bool> exit_flag(false);
-SOCKET sock;
-SOCKET client_socket;
+bool exit_flag = false;
+int sock;
+int client_socket;
 
 void process_data(const std::string& s) {
     if (function3(s.c_str())) {
-        std::cout << "Data received!" << std::endl;
+        std::cout << "[-] SUCCESS: Data is valid ( " << s << " )" << std::endl;
     }
     else {
-        std::cerr << "Error: received invalid data: ( " << s << " )" <<  std::endl;
+        std::cerr << "[-] ERROR: Data is invalid ( " << s << " )" <<  std::endl;
     }
 }
 
@@ -39,13 +38,18 @@ void server() {
     while(!exit_flag) {
         char buffer[1024];
         int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+        
         if (bytes_received <= 0) {
             continue;
         }
         std::string s(buffer, bytes_received);
         process_data(s);
     }   
-    closesocket(client_socket);
+    #ifdef _WIN32
+        closesocket(sock);
+    #else
+        close(sock);
+    #endif
 }
 
 int main() {
@@ -58,36 +62,48 @@ int main() {
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
-        std::cerr << "Failed to create socket" << std::endl;
+        std::cerr << "[-] ERROR: Failed to create socket" << std::endl;
         return 1;
     }
 
     sockaddr_in in_addr{};
     in_addr.sin_family = AF_INET;
     in_addr.sin_port = htons(8080);
-    in_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    #ifdef _WIN32
+        in_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    #else
+        inet_pton(AF_INET, "127.0.0.1", &in_addr.sin_addr);
+    #endif
 
     if (in_addr.sin_addr.s_addr == INADDR_NONE) {
-        std::cerr << "Invalid IP address" << std::endl;
+        std::cerr << "[-] ERROR: Invalid IP address" << std::endl;
         return 1;
     }
 
     if (bind(sock, (sockaddr*)&in_addr, sizeof(in_addr)) == -1) {
-        std::cerr << "Bind failed" << std::endl;
+        std::cerr << "[-] ERROR: Bind failed" << std::endl;
         return 1;
     }
 
     if (listen(sock, 5) == -1) {
-        std::cerr << "Listening failed" << std::endl;
+        std::cerr << "[-] ERROR: Listening failed" << std::endl;
         return 1;
     }
     std::cout << "Server is listening..." << std::endl;
 
     sockaddr_in client_addr{};
-    int client_addr_len = sizeof(client_addr);
+    #ifdef _WIN32
+        int client_addr_len;
+    #else
+        unsigned int client_addr_len;
+    #endif
+
+    client_addr_len = sizeof(client_addr);
     while (!exit_flag) {
         client_socket = accept(sock, (sockaddr*)&in_addr, &client_addr_len);
         if(client_socket == -1) {
+            std::cerr << "[-] ERROR: Couldn`t accept connection" << std::endl;
             continue;
         }
 
